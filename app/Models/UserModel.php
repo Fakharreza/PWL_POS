31faac3210c4e39c0ca71c491adf769bf734b245
Reza alamsyah <reza.alamsyah2013@gmail.com>
2024-10-15 23:17:07 +0700

    JS7

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,18 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-class UserModel extends Model
+use Illuminate\Support\Arr;
+use App\Models\LevelModel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
-    protected $table = 'm_user'; 
+    protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    protected $fillable = ['level_id' , 'username' , 'nama' , 'password'];
+    protected $hidden = ['password'];
 
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id' , 'level_id');
+    protected $casts = ['password' => 'hashed'];
 
+    public function level(): BelongsTo{
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+    
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode == $role;
+    }
+    
+    public function getRole()
+    {
+        return $this->level->level_kode;
+    } 
 }
